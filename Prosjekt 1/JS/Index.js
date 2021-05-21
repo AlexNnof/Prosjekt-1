6f8e72e852323944abc8d4543305e38a038986ac
AlexNnof <84562039+AlexNnof@users.noreply.github.com>
2021-05-21 14:09:13 +0200

    Add files via upload

---
+++ Prosjekt 1/JS/Index.js
@@ -0,0 +1,13 @@
+const img = document.getElementById("logo")
+const about = document.getElementById("about")
+
+img.addEventListener("click", function(){
+     location.href = "index.html";
+})
+
+
+
+
+about.addEventListener("click", function(){
+     location.href = "about.html";
+})
